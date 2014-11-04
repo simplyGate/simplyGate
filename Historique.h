acec2a879197c12eff4ccebea1b8037db04534b7
m1jolliv <m1jolliv@enib.fr>
2014-11-04 10:34:52 +0100

    Create Historique.h

---
+++ Historique.h
@@ -0,0 +1,33 @@
+/*
+ * Fichier: Historique.h
+ * Auteurs : JOLLIVET Matthieu / Gauthier Remi / Saliou Thomas
+ * On définit ici une historique
+ * elle contient la date, l'heure de passage ainsi que la plaque 
+ * 
+*/
+
+#ifndef HISTORIQUE
+#define HISTORIQUE
+
+#include "SFML/Graphics.hpp" 
+
+
+
+
+class Historique {
+  
+  private:
+    
+
+    
+  public :
+      
+
+      
+};
+
+
+
+
+
+#endif 
