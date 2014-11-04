94699643d9005eace8d3f67d7c25266e8be7ab3a
r1gauthi <r1gauthi@enib.fr>
2014-11-04 12:35:41 +0100

    Update Image.h

--- Image.h
+++ Image.h
@@ -9,6 +9,7 @@
 #define IMAGE
 
 #include "SFML/Graphics.hpp" 
+#include "iostream"
 
 
 
@@ -16,10 +17,18 @@
 class Image {
   
   private:
-    
+    int _red;
+    int _green;
+    int _blue;
 
   public :
-      
+    Image(int r, int g, int b) : _red(r), _green(g), _blue(b) {}
+
+    void getRGB(int& r, int& g, int& b) const 
+      { r=_red; g=_green; b=_blue; }
+
+    void setRGB(int r, int g, int b) 
+      { _red=r; _green=g; _blue=b; }
 
       
 };
