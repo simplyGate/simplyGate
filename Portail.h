ed3907d1a0fd1bc6e5401a08ee9ce2f4bc1cb85f
m1jolliv <m1jolliv@enib.fr>
2014-11-04 10:36:53 +0100

    Create Portail.h

---
+++ Portail.h
@@ -0,0 +1,29 @@
+/*
+ * Fichier: Portail.h
+ * Auteurs : JOLLIVET Matthieu / Gauthier Remi / Saliou Thomas
+ * On définit ici les commandes necessaires au fonctionnement du portail
+ * 
+*/
+
+#ifndef PORTAIL
+#define PORTAIL
+
+#include "SFML/Graphics.hpp" 
+
+
+
+class Portail {
+  
+  private:
+    
+
+  public :
+      
+
+};
+
+
+
+
+
+#endif 
