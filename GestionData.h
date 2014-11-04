064c0e520de0032829d7ba3430c1e520faae3fce
m1jolliv <m1jolliv@enib.fr>
2014-11-04 10:34:04 +0100

    Create GestionData.h

---
+++ GestionData.h
@@ -0,0 +1,31 @@
+/*
+ * Fichier: GestionData.h
+ * Auteurs : JOLLIVET Matthieu / Gauthier Remi / Saliou Thomas
+ * On définit ici la section d'administration du programme
+ * elle permet de gerer les plaques autorisées
+ * 
+*/
+
+#ifndef GESTIONDATA
+#define GESTIONDATA
+
+#include "SFML/Graphics.hpp" 
+
+
+
+
+class GestionData {
+  
+  private:
+    
+
+  public :
+      
+
+      
+};
+
+
+
+
+#endif 
