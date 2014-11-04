526679620365d39860a3a26508f8c24cea4be303
m1jolliv <m1jolliv@enib.fr>
2014-11-04 10:36:33 +0100

    Create Image.h

---
+++ Image.h
@@ -0,0 +1,30 @@
+/*
+ * Fichier: Image.h
+ * Auteurs : JOLLIVET Matthieu / Gauthier Remi / Saliou Thomas
+ * On définit ici ce qu'est une Image, composé d'une somme de pixels rouges verts et bleus
+ * 
+*/
+
+#ifndef IMAGE
+#define IMAGE
+
+#include "SFML/Graphics.hpp" 
+
+
+
+
+class Image {
+  
+  private:
+    
+
+  public :
+      
+
+      
+};
+
+
+
+
+#endif 
