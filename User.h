e2e426b16698a313d97760d055a4094e5dd6f3ba
m1jolliv <m1jolliv@enib.fr>
2014-11-04 10:36:02 +0100

    Create User.h

---
+++ User.h
@@ -0,0 +1,32 @@
+/*
+ * Fichier: User.h
+ * Auteurs : JOLLIVET Matthieu / Gauthier Remi / Saliou Thomas
+ * On définit ici ce qu'est un utilisitateur
+ *il possède un ID et un MDP, pour se connectezr au programme
+ * 
+*/
+
+#ifndef USER
+#define USER
+
+#include "SFML/Graphics.hpp" 
+
+
+
+
+class User {
+  
+  private:
+
+    
+  public :
+      
+
+      
+};
+
+
+
+
+
+#endif 
