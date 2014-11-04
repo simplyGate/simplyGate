89a35cd08276b44d75cce5c99f68e1208acf95ef
r1gauthi <r1gauthi@enib.fr>
2014-11-04 12:36:22 +0100

    Update Image.h

--- Image.h
+++ Image.h
@@ -23,10 +23,12 @@ class Image {
 
   public :
     Image(int r, int g, int b) : _red(r), _green(g), _blue(b) {}
-
+                
+                //accesseurs
     void getRGB(int& r, int& g, int& b) const 
       { r=_red; g=_green; b=_blue; }
-
+                
+                //mutateurs
     void setRGB(int r, int g, int b) 
       { _red=r; _green=g; _blue=b; }
 
