cf3d55a9f08967db6c248212c1845f1eb8825847
m1jolliv <m1jolliv@enib.fr>
2014-11-04 10:33:25 +0100

    Create Camera.h

---
+++ Camera.h
@@ -0,0 +1,31 @@
+/*
+ * Fichier: Camera.h
+ * Auteurs : JOLLIVET Matthieu / Gauthier Remi / Saliou Thomas
+ * On définit ici les actions de la camera
+ * elle permet de récuperer un plaque dans une image
+ * 
+*/
+
+#ifndef CAMERA
+#define CAMERA
+
+#include "SFML/Graphics.hpp" 
+
+
+
+
+class Camera {
+  
+  private:
+    
+
+  public :
+      
+
+      
+};
+
+
+
+
+#endif 
